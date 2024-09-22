5321a6696ebea462352f6e7fe166c82d661d029b
Patrul <aliwka9532@gmail.com>
2024-09-22 18:49:14 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let names = ["Ali", "Javid", "Kamran", "undefind", "Emin", "Emil", "undefind", "Rafael"]
+        
+let ul = document.getElementById('names-list')
+
+const showNames = (arr) => { 
+    arr.forEach((name) => {
+        let li = document.createElement('li')
+        li.innerHTML = `${name}`
+        ul.appendChild(li)
+    });
+};
+
+showNames(names)
+   
+document.getElementById('check-btn').addEventListener('click', () => {
+    const listItems = document.querySelectorAll('#names-list li')
+    
+    listItems.forEach((li) => {
+        
+        if (li.innerHTML === 'undefind') {
+            li.innerHTML = 'Будет заменено в будущем' 
+        }
+    })
+})
+
+
+
